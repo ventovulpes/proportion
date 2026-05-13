febb3b01f44cc2eda9a8c8a29393f79d380fafa4
ventovulpes <icybluesquare@gmail.com>
2026-05-13 14:37:02 -0700

    Create static game UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,46 @@ import './App.css'
 
 export default function App() {
   return (
-    <div>proportion.</div>
+    <div className='app'>
+      <Header />
+      <Game />
+   </div>
+  )
+}
+
+function Header() {
+  return (
+    <h1>proportion.</h1>
+  )
+}
+
+function Game() {
+  return (
+    <div className='game'>
+      <Fraction />
+      <BoxArea />
+      <SubmitButton />
+    </div>
+  )
+}
+
+function Fraction() {
+  return (
+    <div className='fraction'>
+      <p className='fraction-top'>3</p>
+      <p className='fraction-bottom'>5</p>
+    </div>
+  )
+}
+
+function BoxArea() {
+  return (
+    <div className='box-area'></div>
+  )
+}
+
+function SubmitButton() {
+  return (
+    <button>submit</button>
   )
 }
